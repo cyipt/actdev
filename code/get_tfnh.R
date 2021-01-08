7711fbfd8b520e8802ec4d5a446d073d80edc444
Robin Lovelace <rob00x@gmail.com>
2021-01-08 17:12:59 +0000

    Test getting geojson for ##2

--- code/get_tfnh.R
+++ code/get_tfnh.R
@@ -5,5 +5,10 @@
 # get geojson for one site, e.g.
 # https://gist.github.com/joeytalbot/9cf8c15ce9142609c9611abf9fb7d8bc
 
+u = "https://gist.github.com/joeytalbot/9cf8c15ce9142609c9611abf9fb7d8bc"
+u_json = "https://gist.github.com/joeytalbot/9cf8c15ce9142609c9611abf9fb7d8bc/raw/6ec5cf8ae19db6366db60ed242fc8a9c341979ef/map.geojson"
+boundary = sf::read_sf(u_json)
+mapview::mapview(boundary)
+
 # workflow to get them from all sites, possibly using this table:
 # https://gist.github.com/aspeakman/989994ec957da57640610d9aa1cd0939
