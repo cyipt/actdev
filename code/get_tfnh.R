c1176013f4f8bfc18745009f56ec1d4e2ddcae3f
Robin Lovelace <rob00x@gmail.com>
2021-01-08 17:07:44 +0000

    Add set-up comments

--- code/get_tfnh.R
+++ code/get_tfnh.R
@@ -2,3 +2,8 @@
 
 # see https://github.com/cyipt/actdev/issues/22
 
+# get geojson for one site, e.g.
+# https://gist.github.com/joeytalbot/9cf8c15ce9142609c9611abf9fb7d8bc
+
+# workflow to get them from all sites, possibly using this table:
+# https://gist.github.com/aspeakman/989994ec957da57640610d9aa1cd0939
