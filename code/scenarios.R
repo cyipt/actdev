5f955a5c8290f07cd1cdf272f732c6962f6b4638
Robin Lovelace <rob00x@gmail.com>
2021-01-11 14:54:37 +0000

    Add scenario of cycling uptake

--- code/scenarios.R
+++ code/scenarios.R
@@ -1,6 +1,13 @@
 # Aim: generate scenarios of change associated with new developments
 
 library(tidyverse)
+library(tmap)
+tmap_mode("view")
+
+# set-up and parameters ---------------------------------------------------
+
+max_length = 20000 # maximum length of desire lines in m
+site_number = 16   # which site to look at (can change)
 
 # input data --------------------------------------------------------------
 
@@ -15,13 +22,14 @@ sites = sf::st_read(u)
 
 # select site of interest
 mapview::mapview(sites)
-site_number = 16
 site = sites[site_number, ]
 mapview::mapview(site)
 zones_touching_site = zones_msoa_national[site, , op = sf::st_intersects]
 mapview::mapview(zones_touching_site)
 
 # Generate desire lines ---------------------------------------------------
+
+# todo: route to site - Joey has done can add code here
 
 od_site = od %>% 
   filter(geo_code1 %in% zones_touching_site$geo_code) %>% 
@@ -31,7 +39,33 @@ od_site = od %>%
 
 desire_lines_site = od::od_to_sf(x = od_site, z = centroids_msoa)
 mapview::mapview(desire_lines_site)
+desire_lines_site$length = stplanr::geo_length(desire_lines_site)
+desire_lines_site = desire_lines_site %>% 
+  filter(length <= max_length) %>% 
+  mutate(pwalk_commute_base = foot/all) %>% 
+  mutate(pcycle_commute_base = bicycle/all) %>% 
+  mutate(pdrive_commute_base = car_driver/all) %>% 
+  mutate(gradient = 0)
 
+# todo: add PT
+
+# mapview::mapview(desire_lines_site)
+co = c("pwalk_commute_base", "pcycle_commute_base", "pdrive_commute_base")
+b = tmaptools::bb(desire_lines_site, ext = 0.2)
+tm_shape(desire_lines_site, bbox = b) +
+  tm_lines(lwd = "all", scale = 9, col = co, palette = "viridis")
+
+# todo: add empirical data on 'new homes' effect
+# todo: add route data
+desire_lines_site = desire_lines_site %>% 
+  mutate(pcycle_commute_godutch = pct::uptake_pct_godutch_2020(distance = length, gradient = gradient))
+plot(desire_lines_site$length, desire_lines_site$pcycle_commute_godutch, cex = desire_lines_site$all / 100)
+
+
+desire_lines_site = desire_lines_site %>% 
+  mutate(pwalk_commute_base = foot/all) %>% 
+  mutate(pcycle_commute_base = bicycle/all) %>% 
+  mutate(pdrive_commute_base = car_driver/all) 
 
 
 
