871a2c1e44c6ba9805f35fa6bf05b772fe43df8b
Robin Lovelace <rob00x@gmail.com>
2021-01-11 15:07:31 +0000

    Complete demo for #23

--- code/scenarios.R
+++ code/scenarios.R
@@ -58,14 +58,31 @@ tm_shape(desire_lines_site, bbox = b) +
 # todo: add empirical data on 'new homes' effect
 # todo: add route data
 desire_lines_site = desire_lines_site %>% 
-  mutate(pcycle_commute_godutch = pct::uptake_pct_godutch_2020(distance = length, gradient = gradient))
+  mutate(pcycle_commute_godutch = pct::uptake_pct_godutch_2020(distance = length, gradient = gradient)) %>% 
+  mutate(pwalk_commute_godutch = case_when(
+    length <= 2000 ~ pwalk_commute_base + 0.1, # 10% shift walking
+    TRUE ~ pwalk_commute_base 
+    )
+    )  
+plot(desire_lines_site$pwalk_commute_godutch, desire_lines_site$pwalk_commute_base, cex = desire_lines_site$all / 100)
 plot(desire_lines_site$length, desire_lines_site$pcycle_commute_godutch, cex = desire_lines_site$all / 100)
+points(desire_lines_site$length, desire_lines_site$pwalk_commute_godutch, cex = desire_lines_site$all / 100, col = "blue")
 
 
-desire_lines_site = desire_lines_site %>% 
-  mutate(pwalk_commute_base = foot/all) %>% 
-  mutate(pcycle_commute_base = bicycle/all) %>% 
-  mutate(pdrive_commute_base = car_driver/all) 
+desire_lines_scenario = desire_lines_site %>% 
+  mutate(bicycle_commute_godutch = all * pcycle_commute_godutch) %>% 
+  mutate(walk_commute_godutch = all * pwalk_commute_godutch) %>% 
+  mutate(car_commute_godutch = car_driver + (bicycle - bicycle_commute_godutch) +
+           (foot - walk_commute_godutch)) %>% 
+  mutate(pcar_commute_godutch = car_commute_godutch / all)
 
+co_dutch = c("walk_commute_godutch", "bicycle_commute_godutch", "car_commute_godutch")
+co_pdutch = c("pwalk_commute_godutch", "pcycle_commute_godutch", "pcar_commute_godutch")
+
+tm_shape(desire_lines_scenario, bbox = b) +
+  tm_lines(lwd = "all", scale = 9, col = co_pdutch, palette = "viridis")
+
+tm_shape(desire_lines_scenario, bbox = b) +
+  tm_lines(lwd = "all", scale = 9, col = co_dutch, palette = "viridis")
 
 
