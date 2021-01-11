68cd427da532bc4a40d7719f491fd5272b0774a9
Robin Lovelace <rob00x@gmail.com>
2021-01-11 16:22:59 +0000

    Add osm script for #15

---
+++ code/tests/osmextract.R
@@ -0,0 +1,11 @@
+# Aim get pois from osm in a way scales
+
+remotes::install_github("itsleeds/osmextract")
+
+library(osmextract)
+
+# define study area
+region_name = "cambridgeshire"
+osm_points = o
+
+# supermarkets
