ee04e1e517414737c59e0caf988e9aa0f9891e35
Robin Lovelace <rob00x@gmail.com>
2021-01-11 14:33:57 +0000

    Add desire lines code for #23

--- code/scenarios.R
+++ code/scenarios.R
@@ -18,10 +18,20 @@ mapview::mapview(sites)
 site_number = 16
 site = sites[site_number, ]
 mapview::mapview(site)
-zones_touching_site = zones_msoa_national[site, ]
+zones_touching_site = zones_msoa_national[site, , op = sf::st_intersects]
 mapview::mapview(zones_touching_site)
 
 # Generate desire lines ---------------------------------------------------
+
+od_site = od %>% 
+  filter(geo_code1 %in% zones_touching_site$geo_code) %>% 
+  filter(geo_code2 %in% centroids_msoa$msoa11cd) %>% 
+  filter(geo_code1 != geo_code2) # note: not accounting for intrazonal flows
+# intra-zonal flows could added later
+
+desire_lines_site = od::od_to_sf(x = od_site, z = centroids_msoa)
+mapview::mapview(desire_lines_site)
+
 
 
 
