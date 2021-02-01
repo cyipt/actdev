681eef373db84efd03f43d0689f755ab5a7bc2de
joeytalbot <joeytalbot85@gmail.com>
2021-02-01 13:31:51 +0000

    mode split summaries

--- code/mode-split-summary.R
+++ code/mode-split-summary.R
@@ -1,13 +1,19 @@
 # Mode split summary by distance ------------------------------------------	
-
+library(tidyverse)
 # The Go Dutch scenario does not work for routes with a distance > 30km	
 # mapview(desire_lines_scenario %>% filter(length >+ 30000))	
 
-sum_total = sum(desire_lines_scenario$all_commute_base)	
+site_name = "great-kneighton" 
+file = file.path("data-small", site_name, "all-census-od.csv")
+desire_lines = read_csv(file = file)
 
-mode_split = desire_lines_scenario %>% 	
-  st_drop_geometry() %>%	
-  select(length, all_commute_base, pwalk_commute_base:pdrive_commute_base, pwalk_commute_godutch:pdrive_commute_godutch) %>% 	
+sum_total = sum(desire_lines$all)	
+
+mode_split = desire_lines %>% 	
+  # st_drop_geometry() %>%	
+  select(length, all, foot, bicycle, car_driver, pwalk_commute_base:pdrive_commute_base
+         # , pwalk_commute_godutch:pdrive_commute_godutch
+         ) %>% 	
   mutate(length_cat = case_when(	
     length < 1000 ~ "a.0-1",	
     length < 3000 ~ "b.1-3",	
@@ -19,27 +25,35 @@ mode_split = desire_lines_scenario %>%
     length >= 30000 ~ "h.30+",	
   )) %>% 	
   group_by(length_cat) %>% 	
-  summarise(pwalk_commute_base = weighted.mean(pwalk_commute_base, w = all_commute_base),	
-            pcycle_commute_base = weighted.mean(pcycle_commute_base, w = all_commute_base),	
-            pdrive_commute_base = weighted.mean(pdrive_commute_base, w = all_commute_base),	
-            pwalk_commute_godutch = weighted.mean(pwalk_commute_godutch, w = all_commute_base),	
-            pcycle_commute_godutch = weighted.mean(pcycle_commute_godutch, w = all_commute_base),	
-            pdrive_commute_godutch = weighted.mean(pdrive_commute_godutch, w = all_commute_base),	
-            total = sum(all_commute_base)/ sum_total	
-  ) %>% 	
-  mutate(across(where(is.numeric), round, 2),	
-         pwalk_commute_godutch = ifelse(	
-           length_cat == "h.30+", NA, pwalk_commute_godutch),	
-         pcycle_commute_godutch = ifelse(	
-           length_cat == "h.30+", NA, pcycle_commute_godutch),	
-         pdrive_commute_godutch = ifelse(	
-           length_cat == "h.30+", NA, pdrive_commute_godutch),	
-         pwalk_commute_godutch = ifelse(	
-           length_cat == "g.20-30", NA, pwalk_commute_godutch),	
-         pcycle_commute_godutch = ifelse(	
-           length_cat == "g.20-30", NA, pcycle_commute_godutch),	
-         pdrive_commute_godutch = ifelse(	
-           length_cat == "g.20-30", NA, pdrive_commute_godutch))	
+  summarise(
+    all_commute_base = sum(all),
+    walk_commute_base = sum(foot),
+    cycle_cycle_base = sum(bicycle),
+    drive_commute_base = sum(car_driver),
+    pwalk_commute_base = weighted.mean(pwalk_commute_base, w = all),	
+    pcycle_commute_base = weighted.mean(pcycle_commute_base, w = all),	
+    pdrive_commute_base = weighted.mean(pdrive_commute_base, w = all),	
+    # pwalk_commute_godutch = weighted.mean(pwalk_commute_godutch, w = all),	
+    # pcycle_commute_godutch = weighted.mean(pcycle_commute_godutch, w = all),	
+    # pdrive_commute_godutch = weighted.mean(pdrive_commute_godutch, w = all),	
+    total_proportional = sum(all)/ sum_total	
+  ) %>%
+  mutate(
+    across(where(is.numeric), round, 2),
+    across(all_commute_base:drive_commute_base, round, 0)
+    # , pwalk_commute_godutch = ifelse(
+    #   length_cat == "h.30+", NA, pwalk_commute_godutch),
+    # pcycle_commute_godutch = ifelse(
+    #   length_cat == "h.30+", NA, pcycle_commute_godutch),
+    # pdrive_commute_godutch = ifelse(
+    #   length_cat == "h.30+", NA, pdrive_commute_godutch),
+    # pwalk_commute_godutch = ifelse(
+    #   length_cat == "g.20-30", NA, pwalk_commute_godutch),
+    # pcycle_commute_godutch = ifelse(
+    #   length_cat == "g.20-30", NA, pcycle_commute_godutch),
+    # pdrive_commute_godutch = ifelse(
+    #   length_cat == "g.20-30", NA, pdrive_commute_godutch)
+    )
 
 dsn = file.path("data-small", site_name, "mode-split.csv")	
 readr::write_csv(mode_split, file = dsn)	
