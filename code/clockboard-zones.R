e14cc27d54eace21d2cd96c73b9d12cbfb25ba85
Robin Lovelace <rob00x@gmail.com>
2021-02-11 01:44:14 +0000

    Add aggregation step and column name placeholders for #46

--- code/clockboard-zones.R
+++ code/clockboard-zones.R
@@ -28,10 +28,39 @@ site_centroid = site_area %>%
 zones_concentric = zonebuilder::zb_zone(site_centroid, n_circles = 3)
 
 # plot(zones_concentric)
-routes_fast_broken = stplanr::line_breakup(l = routes_fast, z = zones_concentric) # slow
-mapview::mapview(routes_fast_broken[1:9, ])
-mapview::mapview(routes_fast_broken) +
-  mapview::mapview(zones_concentric)
+library(sf)
+# routes_fast_broken = stplanr::line_segment(routes_fast, segment_length = 500) # fails
+routes_fast_seg = routes_fast %>% sf::st_cast("LINESTRING")
+routes_fast_seg$segment_length = stplanr::geo_length(routes_fast_seg)
+routes_fast_cents = routes_fast_seg %>%
+  sf::st_centroid() %>% 
+  mutate(
+    dist_cycled_base = cycle_base * segment_length,
+    dist_cycled_dutch = cycle_godutch * segment_length,
+    ) %>% 
+  sf::st_join(zones_concentric[1])
+
+zone_df = routes_fast_cents %>% 
+  sf::st_drop_geometry() %>% 
+  group_by(label) %>% 
+  summarise(
+    busyness_cycle_base = weighted.mean(mean_busyness, dist_cycled_base),
+    busyness_cycle_dutch = weighted.mean(mean_busyness, dist_cycled_dutch),
+    circuity_cycle_fast = NA,
+    circuity_cycle_balanced = NA,
+    circuity_cycle_quiet = NA,
+    circuity_walk = NA,
+    quietness_diversion = NA
+  )
+
+zones_db = left_join(zones_concentric, zone_df)
+names(zones_db)
+
+mapview::mapview(zones_db["busyness_cycle_base"])
+
+
+mapview::mapview(zones_concentric) +
+  mapview::mapview(routes_fast_broken[1:99, ])
 route_centroids = sf::st_centroid(routes) %>% 
   select(all_commute_base, mean_busyness) %>% 
   sf::st_join(zones_concentric) %>% 
