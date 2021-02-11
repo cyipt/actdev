05aa9030c235d2e2c7abc6855199b109c28c7c2e
Robin Lovelace <rob00x@gmail.com>
2021-02-11 01:50:46 +0000

    Tidy up clockboard code ready for #46

--- code/clockboard-zones.R
+++ code/clockboard-zones.R
@@ -15,7 +15,6 @@ routes_fast = sf::read_sf(file.path(path, "routes-fast.geojson"))
 routes_quiet = sf::read_sf(file.path(path, "routes-quiet.geojson"))
 routes_walk = sf::read_sf(file.path(path, "routes-walk.geojson"))
 
-
 # zonebuilder zones -------------------------------------------------------
 distances = c(0, zonebuilder::zb_100_triangular_numbers[1:9])
 summary(cut(routes_fast$length / 1000, distances))
@@ -27,8 +26,6 @@ site_centroid = site_area %>%
 
 zones_concentric = zonebuilder::zb_zone(site_centroid, n_circles = 3)
 
-# plot(zones_concentric)
-library(sf)
 # routes_fast_broken = stplanr::line_segment(routes_fast, segment_length = 500) # fails
 routes_fast_seg = routes_fast %>% sf::st_cast("LINESTRING")
 routes_fast_seg$segment_length = stplanr::geo_length(routes_fast_seg)
@@ -58,30 +55,11 @@ names(zones_db)
 
 mapview::mapview(zones_db["busyness_cycle_base"])
 
+sf::st_precision(zones_db) = 10000
 
-mapview::mapview(zones_concentric) +
-  mapview::mapview(routes_fast_broken[1:99, ])
-route_centroids = sf::st_centroid(routes) %>% 
-  select(all_commute_base, mean_busyness) %>% 
-  sf::st_join(zones_concentric) %>% 
-  sf::st_drop_geometry() %>% 
-  group_by(segment_id) %>% 
-  summarise(n = sum(first(all_commute_base)), busyness = mean(routes$mean_gradient)) 
+db_file = file.path(path, "dartboard.geojson")
+if(file.exists(db_file)) file.remove(db_file)
+sf::write_sf(zones_db, db_file)
 
-zones_concentric_joined = left_join(zones_concentric, y = route_centroids  )
-zones_concentric_joined$busyness = zones_concentric_joined$busyness + runif(nrow(zones_concentric))
 
-zones_concentric_joined %>% 
-  ggplot() +
-  geom_sf(aes(fill = busyness)) +
-  scale_fill_gradient(low = "green", high = "red")
-
-library(tmap)
-tm_shape(zones_concentric_joined) +
-  tm_polygons("busyness") +
-  tm_scale_bar()
-
-sf::st_precision(zones_concentric_joined) = 10000
-zones_concentric_joined = sf::st_set_precision(zones_concentric_joined, 100000)
-sf::write_sf(zones_concentric_joined, "data-small/great-kneighton/dartboard-1-3-6km.geojson")  
-head(readLines("data-small/great-kneighton/dartboard.geojson"))
+# head(readLines("data-small/great-kneighton/dartboard.geojson"))
