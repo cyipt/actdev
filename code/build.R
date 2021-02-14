0e41516ecb69d3dcd0ed8f4bdb76ddc0e8991f03
Robin Lovelace <rob00x@gmail.com>
2021-02-14 11:38:36 +0000

    Start building all sites agan

--- code/build.R
+++ code/build.R
@@ -19,7 +19,7 @@ set.seed(2021)
 # site_names_to_build = "kidbrooke-village"
 site_names_to_build = sites %>% 
   # filter(!str_detect(string = site_name, pattern = "allerton-bywater")) %>% 
-  slice(1:5) %>%
+  # slice(1:5) %>%
   pull(site_name)
 data_dir = "data-small" # for test sites
 # dir.create(data_dir)
