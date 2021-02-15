3a7190cbee4b344507acc86c4c98d94f9df5fbb4
Robin Lovelace <rob00x@gmail.com>
2021-02-15 09:12:57 +0000

    Scenario jsons for all sites

--- code/build.R
+++ code/build.R
@@ -3,20 +3,17 @@
 library(tidyverse)
 library(sf)
 library(stplanr)
-
 max_length = 20000 # maximum length of desire lines in m
 household_size = 2.3 # mean UK household size at 2011 census
 min_flow_routes = 10 # threshold above which OD pairs are included
 region_buffer_dist = 2000
 large_area_buffer = 500
-
 sites = sf::read_sf("data-small/all-sites.geojson")
+source("code/build-setup.R") # national data
 
-source("code/build-setup.R")
-
-set.seed(2021)
-
+# build aggregate scenarios ----------------------------------------------
 # site_names_to_build = "kidbrooke-village"
+set.seed(2021) # reproducibility
 site_names_to_build = sites %>% 
   # fails on the abstreet scenarios for some reason...
   filter(!str_detect(string = site_name, pattern = "bath|ebb|handf|tyersal-lane")) %>%
@@ -35,12 +32,9 @@ for(site_name in site_names_to_build) {
   })
 }
 
-# zip(zipfile = "data-sites-2021-02-08.zip", files = "data-sites")
-
-# Add json files for abstreet
+# Add json files for abstreet ---------------------------------------------
 # site_directories = list.dirs(data_dir)[-1]
 # site_names_to_build = gsub(pattern = "data-small/", replacement = "", x = site_directories)
-
 for(site_name in site_names_to_build) {
   message("Building for ", site_name)
   suppressMessages({
@@ -50,3 +44,16 @@ for(site_name in site_names_to_build) {
   })
 }
 
+# Generate 'clockboard' data ----------------------------------------------
+
+for(site_name in site_names_to_build) {
+  message("Building for ", site_name)
+  suppressMessages({
+    suppressWarnings({
+      source("code/clockboard-zones.R")
+    })
+  })
+}
+
+
+# zip(zipfile = "data-sites-2021-02-08.zip", files = "data-small")
--- code/clockboard-zones.R
+++ code/clockboard-zones.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 remotes::install_github("zonebuilders/zonebuilder") 
 setwd("~/cyipt/actdev")
-
 if(!exists("site_name")) site_name = "chapelford"
 sites = sf::read_sf("data-small/all-sites.geojson")
 site = sites[sites$site_name == site_name, ]
@@ -10,10 +9,11 @@ path = file.path("data-small", site_name)
 # input data: we should probably have naming conventions for these
 list.files(path)
 site_area = sf::read_sf(file.path(path, "site.geojson"))
-desire_lines = sf::read_sf(file.path(path, "desire-lines-few.geojson"))	
-routes_fast = sf::read_sf(file.path(path, "routes-fast.geojson"))
-routes_quiet = sf::read_sf(file.path(path, "routes-quiet.geojson"))
-routes_walk = sf::read_sf(file.path(path, "routes-walk.geojson"))
+desire_lines = sf::read_sf(file.path(path, "desire-lines-many.geojson"))	
+routes_fast = readRDS(file.path(path, "routes_fast.Rds"))
+routes_balanced = readRDS(file.path(path, "routes_balanced.Rds"))
+routes_quiet = readRDS(file.path(path, "routes_quiet.Rds"))
+routes_walk = readRDS(file.path(path, "routes_walk.Rds"))
 
 # zonebuilder zones -------------------------------------------------------
 distances = c(0, zonebuilder::zb_100_triangular_numbers[1:9])
@@ -23,39 +23,98 @@ summary(cut(routes_fast$length / 1000, distances))
 source("code/tests/color_palette.R")
 site_centroid = site_area %>% 
   sf::st_centroid()
-
 zones_concentric = zonebuilder::zb_zone(site_centroid, n_circles = 3)
 
-# routes_fast_broken = stplanr::line_segment(routes_fast, segment_length = 500) # fails
-routes_fast_seg = routes_fast %>% sf::st_cast("LINESTRING")
-routes_fast_seg$segment_length = stplanr::geo_length(routes_fast_seg)
-routes_fast_cents = routes_fast_seg %>%
-  sf::st_centroid() %>% 
+mapview::mapview(zones_concentric) +
+  routes_fast %>% sample_n(1000)
+
+# Rename Go Dutch vars and joine with fast segs ---------------------------
+
+routes_quiet_df = routes_quiet %>% 
+  sf::st_drop_geometry() %>% 
   mutate(
-    dist_cycled_base = cycle_base * segment_length,
-    dist_cycled_dutch = cycle_godutch * segment_length,
+    busyness = busynance / distances,
+    dist_cycled_base = cycle_base * distances,
+    dist_travelled_all = all_base * distances
     ) %>% 
+  group_by(geo_code1, geo_code2) %>%
+  summarise(
+    # mean_gradient = weighted.mean(gradient_smooth, distances),
+    # max_gradient = max(gradient_smooth),
+    length_quiet = mean(length),
+    busyness_mean_quiet = weighted.mean(busyness, distances),
+    length_quiet = mean(length),
+    diversion_factor_quiet = mean(length) / mean(crow_fly_distance),
+    route_dist_cycled_quiet = mean(cycle_base) * mean(length)
+    )
+
+# Calculate Go Dutch cycling levels ---------------------------------------
+
+routes_fast_df = routes_fast_seg %>% 
+  sf::st_drop_geometry() %>% 
+  mutate(busyness = busynance / distances) %>% 
+  group_by(geo_code1, geo_code2) %>%
+  summarise(
+    # mean_gradient = weighted.mean(gradient_smooth, distances),
+    # max_gradient = max(gradient_smooth),
+    length_fast = mean(length),
+    busyness_mean = weighted.mean(busyness, distances), 
+    busyness_90th = quantile(x = busyness, probs = 0.9),
+    busyness_max = max(busyness),
+    pcycle_godutch_uptake = pct::uptake_pct_godutch_2020(distance = mean(length), gradient = weighted.mean(gradient_smooth, distances)),
+    cycle_godutch_additional = pcycle_godutch_uptake * mean(drive_base),
+    cycle_godutch = mean(cycle_base) + cycle_godutch_additional,
+    pcycle_godutch = cycle_godutch / mean(all_base),
+    drive_godutch = mean(drive_base) - cycle_godutch_additional, # ensure totals add up
+    diversion_factor = mean(length) / mean(crow_fly_distance),
+    route_dist_cycled_base = mean(cycle_base) * mean(length),
+    route_dist_cycled_dutch = mean(cycle_base) * mean(length)
+  )
+
+nrow(routes_fast_df)
+mean(routes_fast_df$busyness_90th)
+mean(routes_fast_df$busyness_max)
+
+routes_fast_joined = left_join(routes_fast, routes_fast_df)
+routes_fast_joined2 = left_join(routes_fast_joined, routes_quiet_df)
+summary(routes_fast_joined)
+summary(routes_fast_joined2)
+
+# routes_fast_broken = stplanr::line_segment(routes_fast, segment_length = 500) # fails
+routes_fast_cents = routes_fast_joined2 %>%
+  sf::st_centroid() %>% 
   sf::st_join(zones_concentric[1])
+nms = grep(pattern = "route_dist", x = names(routes_fast_cents))
+for(i in nms) {
+  dc = routes_fast_cents[[i]]
+  dc[dc == 0] = 1
+  routes_fast_cents[[i]] = dc
+}
 
 zone_df = routes_fast_cents %>% 
   sf::st_drop_geometry() %>% 
   group_by(label) %>% 
   summarise(
-    busyness_cycle_base = weighted.mean(mean_busyness, dist_cycled_base),
-    busyness_cycle_dutch = weighted.mean(mean_busyness, dist_cycled_dutch),
-    circuity_cycle_fast = NA,
+    busyness_cycle_base = weighted.mean(busyness_mean, route_dist_cycled_base),
+    busyness_cycle_dutch = weighted.mean(busyness_mean_quiet, route_dist_cycled_dutch),
+    busyness_cycle_base_quiet = weighted.mean(busyness_mean_quiet, route_dist_cycled_quiet),
+    circuity_cycle_fast = weighted.mean(diversion_factor, route_dist_cycled_base),
     circuity_cycle_balanced = NA,
-    circuity_cycle_quiet = NA,
+    circuity_cycle_quiet = weighted.mean(diversion_factor, route_dist_cycled_base),
     circuity_walk = NA,
-    quietness_diversion = NA
+    quietness_diversion = circuity_cycle_quiet / circuity_cycle_fast
   )
 
 zones_db = left_join(zones_concentric, zone_df)
 names(zones_db)
 
+mapview::mapview(zones_db["busyness_cycle_base"]) +
+  mapview::mapview(routes_fast_cents)
 mapview::mapview(zones_db["busyness_cycle_base"])
 
 sf::st_precision(zones_db) = 10000
+zones_db = zones_db %>% 
+  mutate_if(is.numeric, function(x) round(x, digits = 1))
 
 db_file = file.path(path, "dartboard.geojson")
 if(file.exists(db_file)) file.remove(db_file)
