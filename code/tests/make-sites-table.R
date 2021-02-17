b0f951b5af7a30cac0f5240bb356c1458d54c459
joeytalbot <joeytalbot85@gmail.com>
2021-02-17 17:41:53 +0000

    metrics pactive

--- code/tests/make-sites-table.R
+++ code/tests/make-sites-table.R
@@ -20,17 +20,40 @@ sites_join = inner_join(sites,sites_df %>% select(-dwellings_when_complete))
 
 # prototype code to get site metrics
 i = sites_join$site_name[1]
+sites_join$pactive = NA
 sites_join$pconvertable = NA
+sites_join$pconvertable_near = NA
+sites_join$pfutureactive = NA
 for(i in sites_join$site_name) {
   f = paste0("data-small/", i, "/desire-lines-many.geojson")
-  desire_lines = sf::read_sf(f)
-  drive_trips = sum(desire_lines$drive_base)
+  desire_lines = sf::read_sf(f) 
+  desire_lines = desire_lines %>% filter(purpose == "commute") # take out journeys to towns
+  f = paste0("data-small/", i, "/all-census-od.csv")
+  all_desire_lines = sf::read_sf(f)
+  
+  all_desire_lines = all_desire_lines %>% mutate(across(all:pdrive_base, as.numeric))
+  prop_near = sum(desire_lines$all_base) / sum(all_desire_lines$all) #proportion of commutes that are represented in desire_lines_many 
+  all_trips = sum(all_desire_lines$all)
+  drive_trips = sum(all_desire_lines$car_driver)
+  active_base = sum(all_desire_lines$foot) + sum(all_desire_lines$bicycle)
+  
+  drive_near = sum(desire_lines$drive_base)
   drive_dutch = sum(desire_lines$drive_godutch)
-  pchanged = round(100 * (drive_trips - drive_dutch) / drive_trips)
+  active_near = sum(desire_lines$walk_base) + sum(desire_lines$cycle_base)
+  active_dutch = sum(desire_lines$walk_godutch) + sum(desire_lines$cycle_godutch)
+  
+  pchanged = round(100 * (drive_near - drive_dutch) / drive_trips)  
+  pchanged_ofnear = round(100 * (drive_near - drive_dutch) / drive_near)
+  pactive = round(100 * active_base / all_trips)
+  pfutureactive = round(100 * (active_base + active_dutch - active_near) / all_trips)
   # code to re-add the data to the sites_join table
+  sites_join$pactive[sites_join$site_name == i] = pactive
   sites_join$pconvertable[sites_join$site_name == i] = pchanged
-  message(round(100 * (drive_trips - drive_dutch) / drive_trips), " percent in ", i)
+  sites_join$pconvertable_near[sites_join$site_name == i] = pchanged_ofnear
+  sites_join$pfutureactive[sites_join$site_name == i] = pfutureactive
+  # message(round(100 * (drive_trips - drive_dutch) / drive_trips), " percent in ", i)
 }
+
 
 file.remove("data-small/all-sites.geojson")
 file.remove("all-sites.geojson")
