ecbf5e47fbcbde52c81ef7834d82599ed1ac4d30
joeytalbot <joeytalbot85@gmail.com>
2021-02-19 15:42:31 +0000

    change column names

--- code/tests/make-sites-table.R
+++ code/tests/make-sites-table.R
@@ -21,11 +21,11 @@ sites_join = sf::read_sf("data-small/all-sites.geojson")
 
 # prototype code to get site metrics
 i = sites_join$site_name[1]
-sites_join$pactive = NA
-sites_join$pconvertable = NA
-sites_join$pconvertable_near = NA
-sites_join$pfutureactive = NA
-sites_join$pfutureactive_optimistic = NA
+sites_join$percent_commute_active_base = NA
+sites_join$percent_drive_convertable = NA
+sites_join$percent_mapped_drive_convertable = NA
+# sites_join$percent_scenario_active = NA
+sites_join$percent_commute_active_scenario = NA
 for(i in sites_join$site_name) {
   f = paste0("data-small/", i, "/desire-lines-many.geojson")
   desire_lines = sf::read_sf(f) 
@@ -46,18 +46,18 @@ for(i in sites_join$site_name) {
   
   pchanged = round(100 * (drive_near - drive_dutch) / drive_trips)  
   pchanged_ofnear = round(100 * (drive_near - drive_dutch) / drive_near)
-  pactive = round(100 * active_base / all_trips)
-  pfutureactive = round(100 * (active_base + active_dutch - active_near) / all_trips)
-  # to correct for missing desire lines in small sites: calculate the % by which active travel has increased in the mapped desire lines, then assume it increases by the same proportion in unmapped desire lines (yes this is overly optimistic because the unmapped ones will be longer, but at least it evens things out between sites) 
-  pactive_increase = active_dutch / active_near
-  pfutureactive_optimistic = round(100 * (active_base / all_trips * pactive_increase))
+  percent_commute_active_base = round(100 * active_base / all_trips)
+  # percent_scenario_active = round(100 * (active_base + active_dutch - active_near) / all_trips)
+  # to correct for missing desire lines in small sites: calculate the % by which active travel has increased in the mapped desire lines, then assume it increases by the same proportion in unmapped desire lines (yes this is slightly optimistic because the unmapped ones will be longer, but at least it evens things out between sites with different populations) 
+  percent_commute_active_increase = active_dutch / active_near
+  percent_commute_active_scenario = round(100 * (active_base / all_trips * percent_commute_active_increase))
   
   # code to re-add the data to the sites_join table
-  sites_join$pactive[sites_join$site_name == i] = pactive
-  sites_join$pconvertable[sites_join$site_name == i] = pchanged
-  sites_join$pconvertable_near[sites_join$site_name == i] = pchanged_ofnear
-  sites_join$pfutureactive[sites_join$site_name == i] = pfutureactive
-  sites_join$pfutureactive_optimistic[sites_join$site_name == i] = pfutureactive_optimistic
+  sites_join$percent_commute_active_base[sites_join$site_name == i] = percent_commute_active_base
+  sites_join$percent_drive_convertable[sites_join$site_name == i] = pchanged
+  sites_join$percent_mapped_drive_convertable[sites_join$site_name == i] = pchanged_ofnear
+  # sites_join$percent_scenario_active[sites_join$site_name == i] = percent_scenario_active
+  sites_join$percent_commute_active_scenario[sites_join$site_name == i] = percent_commute_active_scenario
   # message(round(100 * (drive_trips - drive_dutch) / drive_trips), " percent in ", i)
 }
 
