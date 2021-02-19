694f96a533c3df838c998d9ee77bab1749e9d6cd
joeytalbot <joeytalbot85@gmail.com>
2021-02-19 15:09:10 +0000

    future active travel proportions

--- code/tests/make-sites-table.R
+++ code/tests/make-sites-table.R
@@ -11,12 +11,13 @@ library(tidyverse)
 # write_csv(sites_df, "data-small/all-sites.csv")
 
 # add columns to the csv
-sites_df = read_csv("data-small/all-sites.csv")
-sites_df$planning_url = NA
-write_csv(sites_df, "data-small/all-sites.csv")
-
-sites = sf::read_sf("data-small/all-sites.geojson")
-sites_join = inner_join(sites,sites_df %>% select(-dwellings_when_complete))
+# sites_df = read_csv("data-small/all-sites.csv")
+# sites_df$planning_url = NA
+# write_csv(sites_df, "data-small/all-sites.csv")
+# 
+# sites = sf::read_sf("data-small/all-sites.geojson")
+# sites_join = inner_join(sites,sites_df %>% select(-dwellings_when_complete))
+sites_join = sf::read_sf("data-small/all-sites.geojson")
 
 # prototype code to get site metrics
 i = sites_join$site_name[1]
@@ -24,6 +25,7 @@ sites_join$pactive = NA
 sites_join$pconvertable = NA
 sites_join$pconvertable_near = NA
 sites_join$pfutureactive = NA
+sites_join$pfutureactive_optimistic = NA
 for(i in sites_join$site_name) {
   f = paste0("data-small/", i, "/desire-lines-many.geojson")
   desire_lines = sf::read_sf(f) 
@@ -46,11 +48,16 @@ for(i in sites_join$site_name) {
   pchanged_ofnear = round(100 * (drive_near - drive_dutch) / drive_near)
   pactive = round(100 * active_base / all_trips)
   pfutureactive = round(100 * (active_base + active_dutch - active_near) / all_trips)
+  # to correct for missing desire lines in small sites: calculate the % by which active travel has increased in the mapped desire lines, then assume it increases by the same proportion in unmapped desire lines (yes this is overly optimistic because the unmapped ones will be longer, but at least it evens things out between sites) 
+  pactive_increase = active_dutch / active_near
+  pfutureactive_optimistic = round(100 * (active_base / all_trips * pactive_increase))
+  
   # code to re-add the data to the sites_join table
   sites_join$pactive[sites_join$site_name == i] = pactive
   sites_join$pconvertable[sites_join$site_name == i] = pchanged
   sites_join$pconvertable_near[sites_join$site_name == i] = pchanged_ofnear
   sites_join$pfutureactive[sites_join$site_name == i] = pfutureactive
+  sites_join$pfutureactive_optimistic[sites_join$site_name == i] = pfutureactive_optimistic
   # message(round(100 * (drive_trips - drive_dutch) / drive_trips), " percent in ", i)
 }
 
