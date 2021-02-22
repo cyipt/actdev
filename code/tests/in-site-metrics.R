5d6623ca01f96e7d50fb4448b1869e49fe2cd2fc
Robin Lovelace <rob00x@gmail.com>
2021-02-22 13:39:57 +0000

    Add placeholder for #93

---
+++ code/tests/in-site-metrics.R
@@ -0,0 +1,17 @@
+# Aim: calculate in-site metrics
+
+# Question: should we base this on OSM data or routing data?
+# It's quicker and less resource intensive to calculate 
+# these metrics using a simple routing approach, reducing
+# the computational requirements associated with bulk
+# import and processing of OSM, although that 2nd 
+# approach allows more detailed info to be extracted
+
+
+# Approach1: quicker and easier -------------------------------------------
+
+
+
+# Approach 2: use data from osmextract ------------------------------------
+
+
