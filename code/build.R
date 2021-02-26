3dd85ae2e463ee635aea3528ca9fc23223e8b03e
Robin Lovelace <rob00x@gmail.com>
2021-02-26 15:13:24 +0000

    Update with fewer decimal places

--- code/build.R
+++ code/build.R
@@ -101,7 +101,8 @@ setdiff(n2, n1)
 (in_original_not_all = n1[!n1 %in% n2])
 (extra_vars = n2[!n2 %in% n1])
 site_original_not_in_all = site_original[c("site_name", in_original_not_all)]
-site_new = left_join(site_original_not_in_all, all_sites %>% sf::st_drop_geometry())
+site_new = left_join(site_original_not_in_all, all_sites %>% sf::st_drop_geometry()) %>% 
+  mutate_if(is.numeric, function(x) round(x, digits = 2))
 file.remove("data-small/allerton-bywater/site.geojson")
 sf::write_sf(site_new, "data-small/allerton-bywater/site.geojson")
 
