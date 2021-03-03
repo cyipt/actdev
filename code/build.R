ca6367343643ae9e9573d6e5ae20c5f1e5abb595
Robin Lovelace <rob00x@gmail.com>
2021-03-03 00:21:00 +0000

    Update poundbury trip attractors

--- code/build.R
+++ code/build.R
@@ -16,13 +16,12 @@ source("code/build-setup.R") # national data
 set.seed(2021) # reproducibility
 site_names_to_build = sites %>% 
   # fails on the abstreet scenarios for some reason...
-  # filter(str_detect(string = site_name, pattern = "bath|ebb|handf|tyersal-lane")) %>% # I don't know why these sites were excluded
+  filter(str_detect(string = site_name, pattern = "kneighton|allert|pound")) %>% # I don't know why these sites were excluded
   # slice(1:35) %>%
   pull(site_name)
 
 data_dir = "data-small" # for test sites
 disaggregate_desire_lines = TRUE
-
 for(site_name in site_names_to_build) {
   message("Building for ", site_name)
   suppressMessages({
