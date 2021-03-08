75bb616b5a9cc9b4786510655bef0cb9270c7b25
Robin Lovelace <rob00x@gmail.com>
2021-03-08 08:54:13 +0000

    Prep for #117

--- code/build.R
+++ code/build.R
@@ -16,7 +16,7 @@ source("code/build-setup.R") # national data
 set.seed(2021) # reproducibility
 site_names_to_build = sites %>% 
   # uncomment the following lines to omit of test for subset of sites
-  filter(!str_detect(string = site_name, pattern = "tyersal")) %>%
+  # filter(!str_detect(string = site_name, pattern = "tyersal")) %>%
   # filter(str_detect(string = site_name, pattern = "kneighton|allert|pound")) %>%
   pull(site_name)
 
