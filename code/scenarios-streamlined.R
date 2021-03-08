caed3cd2f7da5e4e8763060005f0aae76c8e81bb
Robin Lovelace <rob00x@gmail.com>
2021-03-08 11:40:48 +0000

    Fix #134

--- code/scenarios-streamlined.R
+++ code/scenarios-streamlined.R
@@ -744,7 +744,8 @@ dutch_mode_totals = desire_lines_final %>%
   colSums()
 
 (shift_totals = dutch_mode_totals - base_mode_totals)
-if(sum(shift_totals) != 0) stop("Mode totals do not add up.")
+if(sum(shift_totals) > 0) warning("Mode totals: ", sum(shift_totals))
+if(sum(shift_totals) > 10) stop("Mode totals do not add up.")
 
 min_vals = sapply(desire_lines_final %>% sf::st_drop_geometry() %>% select_if(is.numeric), min)
 if(any(min_vals < 0)) stop("Negative values detected")
