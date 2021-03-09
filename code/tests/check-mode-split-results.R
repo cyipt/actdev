354be18e2bf3e758c26d2a0ffcc9a4864d157e4a
Robin Lovelace <rob00x@gmail.com>
2021-03-09 18:02:12 +0000

    Close #141

--- code/tests/check-mode-split-results.R
+++ code/tests/check-mode-split-results.R
@@ -82,11 +82,13 @@ sum(od_sample$all) / sum(od_national$all) # 40% trips
 
 desire_lines_sample = od::od_to_sf(od_sample, centroids_msoa)
 desire_lines_sample$distance = stplanr::geo_length(desire_lines_sample)
-dist_breaks = c(seq(from = 0.2, to = 4.5, by = 0.4),  seq(from = 5, to = 21, by = 2)) * 1000
+dist_breaks = c(seq(from = 0.2, to = 4.5, by = 0.2),  seq(from = 5, to = 21, by = 2)) * 1000
+
+summary(desire_lines_sample$distance)
 
 desire_lines_modesplit = desire_lines_sample %>%
   filter(distance < 20000) %>% 
-  filter(distance > 0.2) %>% 
+  filter(distance > 300) %>% 
   mutate(
   distance_band_fine = cut(distance, dist_breaks),
   pwalk_base = foot / all,
@@ -133,6 +135,7 @@ desire_lines_summary$Mode[grepl(pattern = "drive", x = desire_lines_summary$Scen
 desire_lines_summary$Scenario[grepl(pattern = "active", x = desire_lines_summary$ScenarioMode)] = "Go Active"
 desire_lines_summary$Mode = factor(desire_lines_summary$Mode, levels = c("Drive", "Cycle", "Walk"))
 
+# first pass
 desire_lines_summary %>% 
   ggplot() +
   # geom_line(aes(distance, value, colour = Mode)) %>% 
@@ -141,4 +144,65 @@ desire_lines_summary %>%
   scale_y_continuous(labels = scales::percent)
 
 ggsave("data-small/scenario-overview.png")
+
+
+desire_lines_summary = desire_lines_modesplit %>% 
+  sf::st_drop_geometry() %>% 
+  group_by(distance_band_fine) %>% 
+  summarise(
+    distance = mean(distance) / 1000,
+    pwalk = sum(foot) / sum(all),
+    pwalk_90th = quantile(foot / all, 0.8),
+    pwalk_10th = quantile(foot / all, 0.2),
+    pwalk_go_active = sum(walk_godutch) / sum(all),
+    pwalk_go_active_90th = quantile(walk_godutch / all, 0.8),
+    pwalk_go_active_10th = quantile(walk_godutch / all, 0.2),
+    pcycle = sum(bicycle) / sum(all),
+    pcycle_90th = quantile(bicycle / all, 0.8),
+    pcycle_10th = quantile(bicycle / all, 0.2),
+    pcycle_go_active = sum(cycle_godutch) / sum(all),
+    pcycle_go_active_90th = quantile(cycle_godutch / all, 0.8),
+    pcycle_go_active_10th = quantile(cycle_godutch / all, 0.2),
+    pdrive = sum(car_driver) / sum(all),
+    pdrive_90th = quantile(car_driver / all, 0.8),
+    pdrive_10th = quantile(car_driver / all, 0.2),
+    pdrive_go_active = sum(drive_godutch) / sum(all),
+    pdrive_go_active_90th = quantile(drive_godutch / all, 0.8),
+    pdrive_go_active_10th = quantile(drive_godutch / all, 0.2),
+  ) %>% 
+  select(matches("p|dist")) %>% 
+  pivot_longer(cols = matches("p"), names_to = "ScenarioMode", values_to = "Proportion")
+
+desire_lines_summary$Mode = NA
+desire_lines_summary$Scenario = "Baseline"
+desire_lines_summary$Mode[grepl(pattern = "cycle", x = desire_lines_summary$ScenarioMode)] = "Cycle"
+desire_lines_summary$Mode[grepl(pattern = "foot|walk", x = desire_lines_summary$ScenarioMode)] = "Walk"
+desire_lines_summary$Mode[grepl(pattern = "drive", x = desire_lines_summary$ScenarioMode)] = "Drive"
+desire_lines_summary$Scenario[grepl(pattern = "active", x = desire_lines_summary$ScenarioMode)] = "Go Active"
+desire_lines_summary$Mode = factor(desire_lines_summary$Mode, levels = c("Drive", "Cycle", "Walk"))
+
+table(desire_lines_summary_all$ScenarioMode)
+desire_lines_q90 = desire_lines_summary %>% filter(str_detect(string = ScenarioMode, pattern = "90th"))
+desire_lines_q10 = desire_lines_summary %>% filter(str_detect(string = ScenarioMode, pattern = "10th"))
+desire_lines_summary = desire_lines_summary %>% filter(!str_detect(string = ScenarioMode, pattern = "th"))
+desire_lines_summary$Proportion_90th = desire_lines_q90$Proportion
+desire_lines_summary$Proportion_10th = desire_lines_q10$Proportion
+
+cols = c("#457b9d", "#90be6d", "#ffd166", "#fe5f55")
+cols = rev(cols[-3])
+
+
+desire_lines_summary %>% 
+  ggplot() +
+  # geom_line(aes(distance, value, colour = Mode)) %>% 
+  geom_line(aes(distance, Proportion, colour = Mode)) +
+  scale_colour_manual(values = cols) +
+  geom_ribbon(aes(distance, ymin = Proportion_10th, ymax = Proportion_90th, fill = Mode), alpha = 0.3) +
+  scale_fill_manual(values = cols) +
+  facet_grid(vars(Scenario)) +
+  scale_y_continuous(labels = scales::percent, limits = c(0, 1)) +
+  scale_x_continuous(name = "Euclidean Distance (km)")
+
+ggsave("data-small/scenario-overview.png")
+
 
