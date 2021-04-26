36be6690736448353cae3620e8d7a265d0ebb604
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-04-26 17:31:46 +0100

    PR comments #165
    Save la districts to geojson folder

--- code/add_site.R
+++ code/add_site.R
@@ -3,12 +3,11 @@
 add_la = function(site) {
   # Download latest ONS "Local Authority Districts in the United Kingdom" data
   download.file(url = 'https://opendata.arcgis.com/datasets/b7fc294e5c8643f5b506acc2122c6880_0.geojson',
-                destfile = 'la_boundaries.geojson',
+                destfile = 'geojsons/la_boundaries.geojson',
                 method = 'wininet')
   # Read input data
-  la_ew = sf::read_sf("la_boundaries.geojson") %>% sf::as_Spatial() %>% sf::st_as_sf()
+  la_ew = sf::read_sf("geojsons/la_boundaries.geojson") %>% sf::as_Spatial() %>% sf::st_as_sf()
   # Create intersection between site and ONS data
   la_site = sf::st_intersection(la_ew, site)
   site$main_local_authority = la_site$LAD19NM
-  file.remove("la_boundaries.geojson")
 }
