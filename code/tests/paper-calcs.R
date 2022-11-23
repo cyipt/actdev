e7e503062eb99576310ec8dfada2ebe8cd181140
Joey Talbot <joeytalbot85@gmail.com>
2022-11-23 10:25:33 +0000

    fig 5

--- code/tests/paper-calcs.R
+++ code/tests/paper-calcs.R
@@ -272,89 +272,99 @@ ggsave(filename = "chapelford-baseline.png", plot = g2, dpi = 300)
 
 # Figure 5 ----------------------------------------------------------------
 
-# use facet_wrap?
-# site_name = "dickens-heath"
-# 
-# path = file.path("data-small", site_name)
-# all_od = read_csv(file.path(path, "all-census-od.csv"))
-# desire_lines = sf::read_sf(file.path(path, "desire-lines-many.geojson"))
-# 
-# # colourscheme
-# cols = c("#457b9d", "#90be6d", "#ffd166", "#fe5f55")
-# 
-# # create combined infographic ---------------------------------------------
-# 
-# get_distance_bands = function(x, distance_band = c(0, zonebuilder::zb_100_triangular_numbers[2:5], 20, 30, 10000)) {
-#   distance_labels = paste0(distance_band[-length(distance_band)], "-", distance_band[-1])
-#   distance_labels = gsub(pattern = "-10000", replacement = "+", distance_labels)
-#   cut(x = x, breaks = distance_band * 1000, labels = distance_labels)
-# }
-# 
-# all_od_new = all_od %>% 
-#   rename(all = all, walk = foot, cycle = bicycle, drive = car_driver, trimode = trimode_base) %>% 
-#   select(geo_code2, all, trimode, walk, cycle, drive, length) %>% 
-#   mutate(across(all:length, as.numeric))
-# 
-# # disaggregated desire lines inside the study area
-# desire_lines_disag = desire_lines %>% 
-#   rename(all = all_base, trimode = trimode_base) %>% 
-#   mutate(distance_band = get_distance_bands(x = length)) %>% 
-#   group_by(distance_band, .drop = FALSE) %>% 
-#   filter(purpose == "commute") %>% 
-#   select(geo_code2, all, trimode, walk_base, cycle_base, drive_base, walk_godutch, cycle_godutch, drive_godutch, length) %>% 
-#   sf::st_drop_geometry()
-# 
-# # get desire lines outside the study area
-# all_dist_outside = all_od_new %>% 
-#   filter(! geo_code2 %in% desire_lines_disag$geo_code2)
-# 
-# # join the lines inside and outside the study area for the two scenarios separately
-# desire_disag_base = desire_lines_disag %>%
-#   select(-c(walk_godutch:drive_godutch)) %>% 
-#   rename(walk = walk_base, cycle = cycle_base, drive = drive_base)
-# 
-# desire_disag_scenario = desire_lines_disag %>%
-#   select(-c(walk_base:drive_base)) %>% 
-#   rename(walk = walk_godutch, cycle = cycle_godutch, drive = drive_godutch)
-# 
-# desire_all_base = bind_rows(desire_disag_base, all_dist_outside)
-# 
-# desire_all_scenario = bind_rows(desire_disag_scenario, all_dist_outside)
-# 
-# # Baseline scenario
-# 
-# mode_split_base = desire_all_base %>%
-#   mutate(distance_band = get_distance_bands(x = length)) %>% 
-#   group_by(distance_band, .drop = FALSE) %>% 
-#   summarise(across(all:drive, sum)) %>% 
-#   mutate(other = all - trimode)
-# 
-# all_dist = mode_split_base %>% 
-#   pivot_longer(cols = c(walk, cycle, drive, other))
-# 
-# all_dist$name = factor(all_dist$name, levels = c("walk", "cycle", "other", "drive"))
-# 
-# # Go Active scenario
-# 
-# mode_split_active = desire_all_scenario %>%
-#   mutate(distance_band = get_distance_bands(x = length)) %>% 
-#   group_by(distance_band, .drop = FALSE) %>% 
-#   summarise(across(all:drive, sum)) %>% 
-#   mutate(other = all - trimode)
-# 
-# all_dist_active = mode_split_active %>% 
-#   pivot_longer(cols = c(walk, cycle, drive, other))
-# 
-# all_dist_active$name = factor(all_dist$name, levels = c("walk", "cycle", "other", "drive"))
-# 
-# g3 = ggplot(all_dist, aes(fill = name, y = value, x = distance_band)) +
-#   geom_bar(position = "stack", stat = "identity"
-#            # , show.legend = FALSE # this makes the bars too wide
-#   ) +
-#   scale_fill_manual(values = cols) +
-#   labs(y = "", x = "Distance band (km)", fill = "") +
-#   theme_minimal() +
-#   ylim(c(0, 500))
-# 
-# ggsave(filename = "dickens-heath.png", plot = g3, dpi = 300)
-# # g1 + theme(text = element_text(size = 20)) 
+site_name = "dickens-heath"
+
+path = file.path("data-small", site_name)
+all_od = read_csv(file.path(path, "all-census-od.csv"))
+desire_lines = sf::read_sf(file.path(path, "desire-lines-many.geojson"))
+
+# colourscheme
+cols = c("#457b9d", "#90be6d", "#ffd166", "#fe5f55")
+
+# create combined infographic ---------------------------------------------
+
+get_distance_bands = function(x, distance_band = c(0, zonebuilder::zb_100_triangular_numbers[2:5], 20, 30, 10000)) {
+  distance_labels = paste0(distance_band[-length(distance_band)], "-", distance_band[-1])
+  distance_labels = gsub(pattern = "-10000", replacement = "+", distance_labels)
+  cut(x = x, breaks = distance_band * 1000, labels = distance_labels)
+}
+
+all_od_new = all_od %>%
+  rename(all = all, walk = foot, cycle = bicycle, drive = car_driver, trimode = trimode_base) %>%
+  select(geo_code2, all, trimode, walk, cycle, drive, length) %>%
+  mutate(across(all:length, as.numeric))
+
+# disaggregated desire lines inside the study area
+desire_lines_disag = desire_lines %>%
+  rename(all = all_base, trimode = trimode_base) %>%
+  mutate(distance_band = get_distance_bands(x = length)) %>%
+  group_by(distance_band, .drop = FALSE) %>%
+  filter(purpose == "commute") %>%
+  select(geo_code2, all, trimode, walk_base, cycle_base, drive_base, walk_godutch, cycle_godutch, drive_godutch, length) %>%
+  sf::st_drop_geometry()
+
+# get desire lines outside the study area
+all_dist_outside = all_od_new %>%
+  filter(! geo_code2 %in% desire_lines_disag$geo_code2)
+
+# join the lines inside and outside the study area for the two scenarios separately
+desire_disag_base = desire_lines_disag %>%
+  select(-c(walk_godutch:drive_godutch)) %>%
+  rename(walk = walk_base, cycle = cycle_base, drive = drive_base)
+
+desire_disag_scenario = desire_lines_disag %>%
+  select(-c(walk_base:drive_base)) %>%
+  rename(walk = walk_godutch, cycle = cycle_godutch, drive = drive_godutch)
+
+desire_all_base = bind_rows(desire_disag_base, all_dist_outside)
+
+desire_all_scenario = bind_rows(desire_disag_scenario, all_dist_outside)
+
+# Baseline scenario
+
+mode_split_base = desire_all_base %>%
+  mutate(distance_band = get_distance_bands(x = length)) %>%
+  group_by(distance_band, .drop = FALSE) %>%
+  summarise(across(all:drive, sum)) %>%
+  mutate(other = all - trimode)
+
+all_dist = mode_split_base %>%
+  pivot_longer(cols = c(walk, cycle, drive, other))
+
+all_dist$name = factor(all_dist$name, levels = c("walk", "cycle", "other", "drive"))
+
+all_dist = all_dist %>% 
+  mutate(scenario = "baseline")
+
+# Go Active scenario
+
+mode_split_active = desire_all_scenario %>%
+  mutate(distance_band = get_distance_bands(x = length)) %>%
+  group_by(distance_band, .drop = FALSE) %>%
+  summarise(across(all:drive, sum)) %>%
+  mutate(other = all - trimode)
+
+all_dist_active = mode_split_active %>%
+  pivot_longer(cols = c(walk, cycle, drive, other))
+
+all_dist_active$name = factor(all_dist$name, levels = c("walk", "cycle", "other", "drive"))
+
+all_dist_active = all_dist_active %>% 
+  mutate(scenario = "active")
+
+all_dist_both = rbind(all_dist, all_dist_active)
+all_dist_both$scenario = factor(all_dist_both$scenario, levels = c("baseline", "active"))
+
+g3 = ggplot(all_dist_both, aes(fill = name, y = value, x = distance_band)) +
+  geom_bar(position = "stack", stat = "identity"
+           # , show.legend = FALSE # this makes the bars too wide
+  ) +
+  scale_fill_manual(values = cols) +
+  labs(y = "", x = "Distance band (km)", fill = "") +
+  facet_wrap(~ scenario) +
+  theme_minimal() +
+  theme(strip.text.x = element_blank()) +
+  ylim(c(0, 500))
+
+ggsave(filename = "dickens-heath.png", plot = g3, dpi = 300)
+# g3 + theme(text = element_text(size = 20))
